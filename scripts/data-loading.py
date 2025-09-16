4d9f35d9a15ac97546bb68187a1053f34de7956d
Chelsea Adetan <adetanchelsea@gmail.com>
2025-09-16 14:45:53 +0100

    Added a fuction to load dataframes into a CSV file.

--- scripts/data-loading.py
+++ scripts/data-loading.py
@@ -1,1 +1,16 @@
+import pandas as pd
+
+def load_to_csv(df, filename):
+    """
+    Save a DataFrame to a CSV file.
+
+    Parameters:
+    df: Pandas DataFrame
+    filename: Output csv file
+    """
+
+    # Loading the dataframe to a csv file
+    df.to_csv(filename, index=False)
+    
+    print(f"Data saved to {filename}")
 
